cb39ed195651f6de56168a92f0a8643c6229e74a
SchattenKanzler <sebastian.kurcz@edu.fh-joanneum.at>
2024-05-15 13:48:45 +0200

    First

---
+++ graphtutorial/appSettings.js
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+// <SettingsSnippet>
+const settings = {
+  'clientId': '78e98ba4-c685-4d10-a9fa-c47f677c27ed',
+  'tenantId': 'common',
+  'graphUserScopes': [
+    'user.read',
+    'mail.read',
+    'mail.send',
+    'calendars.ReadWrite'
+  ]
+};
+
+module.exports = settings;
+// </SettingsSnippet>
